e14e7e24ce9575963876bf835a49dd7d56d3e233
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2017-03-16 14:48:50 +0300

    Initial commit
    Simple app to check emails saved in database from exim

---
+++ emails.php
@@ -0,0 +1,49 @@
+<?php
+include "connection.php";
+
+$res - pg_query($con, "
+select * from emails where 
+upper(sender) like '%" . $_REQUEST['term'] . "%' or 
+
+upper(unnest(addresses)) like '%" . $_REQUEST['term'] . "%' or
+
+position(upper('" . $_REQUEST['term'] . "') in subject) > 0 or  
+
+position(upper('" . $_REQUEST['term'] . "') in body) > 0
+
+");
+?>
+<table summary="" class="table table-striped">
+	<thead>
+			<tr>
+				<th>Date</th>			
+				<th>From</th>			
+				<th>Recipients</th>			
+				<th>Subject</th>			
+				<th>Content</th>			
+			</tr>	
+	</thead>
+	<tbody>
+	<?php
+	if(pg_num_rows($res) == 0 ) {
+		?>
+		<tr>
+			<td colspan="100">No results found</td>		
+		</tr>
+		<?php
+	} else {
+		while($row = pg_fetch_assoc($res)) {
+		?>
+		<tr>
+			<td><?php echo $row['received']; ?></td>	
+			<td><?php echo $row['sender']; ?></td>	
+			<td><?php echo $row['addresses']; ?></td>	
+			<td><?php echo $row['subject']; ?></td>	
+			<td><?php echo $row['subject']; ?></td>	
+		</tr>
+		<?php	
+		}
+	}
+	?>
+	</tbody>
+</table>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+	
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+	<link rel="stylesheet" href="style.css" />
+	
+</head>
+<body>
+	<img src="logo.png" alt="" class="logo">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h2>SEARCH EMAIL DATABASE</h2>
+			</div>
+		</div>	
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-10 text-center">
+				<form class="form form-inline" style="width: 100%">
+					<input type="text" id="term" class="form-control input-sm" style="width: 300px;" placeholder="Type in email address or text to find" />
+					<button type="button" class="btn btn-sm btn-warning">SEARCH</button>
+					<button type="button" class="btn btn-sm btn-info">CLEAR</button>
+				</form>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		<div class="row" id="workspace">
+		
+		</div>	
+	</div>
+	<script type="text/javascript" >
+		$(document).ready(function () {
+			$(".btn-info").click(function () {
+				$("#workspace").empty()
+			})
+			$(".btn-warning").click(function () {
+				$("#workspace").empty().load("email.php?term=" + $("#term").val());
+			})
+		})
+	</script>
+</body>
+</html>
